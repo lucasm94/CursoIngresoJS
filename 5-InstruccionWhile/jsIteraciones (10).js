1cc4f7739e27473926976d99fcc8e063bb1b16de
alumno <alumno@utnfra.com.ar>
2020-02-17 22:03:59 -0300

    clase 5 parte 2

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,44 @@
 function mostrar()
 {
 
-	var contador=0;
 	//declarar contadores y variables 
-	
+	var acumPositivos = 0;
+	var acumNegativos = 0;
+	var contadorPositivos = 0;
+	var contadorNegativos = 0;
+	var contadorPares = 0;
+	var contadorCeros = 0;
+	var numero;
 	var respuesta="si";
 
-	while(respuesta!="no")
-	{
-		
-	
+	while(respuesta!="no" || isNaN(numero))	{
+		numero= parseInt(prompt('ingrese un numero'));
+		if(numero == 0) {
+			contadorCeros++;
+		} else if(numero > 0) {
+			contadorPositivos++;
+			acumPositivos += numero;
+		} else {
+			contadorNegativos++;
+			acumNegativos+=numero;
+		}
+		if(numero % 2 == 0) {
+			contadorPares++;
+		}
+		respuesta = prompt('desea ingresar mas numeros? si/no');
 	}
 
+	var promedioPositivos = acumPositivos / contadorPositivos;
+	var promedioNegativos = acumNegativos / contadorNegativos;
+	var diferenciaPosNeg = acumPositivos - acumNegativos;
 
-
+	console.log('ceros: ' +contadorCeros);
+	console.log('contadorPositivos: ' +contadorPositivos);
+	console.log('contadorNegativos: ' +contadorNegativos);
+	console.log('acumPositivos: ' +acumPositivos);
+	console.log('acumNegativos: ' +acumNegativos);
+	console.log('promedioPositivos: ' +promedioPositivos);
+	console.log('promedioNegativos: ' +promedioNegativos);
+	console.log('diferenciaPosNeg: ' +diferenciaPosNeg);
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,27 @@
 function mostrar()
 {
 
-	var contador=0;
 	// declarar variables
-	
-	var respuesta='si';
+	var maximo;
+	var minimo;
 
-	while(respuesta!='no')
-	{
-		
-	
+	var respuesta='si';
+	var numero = prompt('ingrese un numero');
+	maximo = numero;
+	minimo = numero;
+	respuesta = prompt('desea ingresar mas numeros? si/no');
+	while(respuesta!='no') {
+		numero = prompt('ingrese un numero');
+		if(numero > maximo) {
+			maximo = numero;
+		} else if(numero < minimo) {
+			minimo = numero;
+		}
+		respuesta = prompt('desea ingresar mas numeros? si/no');
 	}
 
-
+	document.getElementById('maximo').value = maximo;
+	document.getElementById('minimo').value = minimo;
 
 
 }//FIN DE LA FUNCIÓN
